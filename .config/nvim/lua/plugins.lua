855cbad66fb47d4b85963ca50b3aae8d4c923168
Nadim Edde Gomez <nadim@eddegomez.org>
2021-06-07 09:19:52 +0100

    feat(nvim): bootstrap installation

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,11 +1,23 @@
+-- Bootstrap packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
+    use '~/git/Apprentice/'
 
     use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
     
-    use {'lewis6991/gitsigns.nvim',requires = { 'nvim-lua/plenary.nvim' }
-}
+    use {'lewis6991/gitsigns.nvim',requires = { 'nvim-lua/plenary.nvim' }}
 
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use 'nvim-treesitter/playground'
