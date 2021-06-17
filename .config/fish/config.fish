5e9b628c11821169937d62dd13f5fd4e0eb1ffe1
Nadim Edde Gomez <nadim@eddegomez.org>
2021-06-17 14:49:09 +0100

    feat(fish): support fzf and tide prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,6 +26,13 @@ set -U fish_pager_color_prefix        'green' '--bold' '--underline'
 set -U fish_pager_color_progress      'brwhite' '--background=cyan'
 
 set -gx EDITOR nvim
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 alias vim=nvim
 alias config='/usr/bin/git --git-dir=/home/nequo/.cfg/ --work-tree=/home/nequo'
-starship init fish | source
+#starship init fish | source
+
+set -g tide_pwd_color_dirs black
+set -g tide_pwd_color_truncated_dirs black
+set -g tide_pwd_color_anchors black
+
+set --export FZF_DEFAULT_OPTS --color=fg:#eaead3,bg:#3e3e3e,hl:#8db98d --color=fg+:#eaead3,bg+:#1e1e1e,hl+:#92c4ec --color=info:#B37576,prompt:#f1e4a8,pointer:#ffaf87 --color=marker:#f1e4a8,spinner:#f1e4a8,header:#6cbbbf --cycle --layout=reverse --border --height=90% --preview-window=wrap
